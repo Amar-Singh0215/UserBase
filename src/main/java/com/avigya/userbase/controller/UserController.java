bbb4c85b620dbda9ae234c47f80f328e34083586
Amar Chauhan <amar.singh.ch@gmail.com>
2024-09-12 22:52:21 +0530

    Updated code

--- src/main/java/com/avigya/userbase/controller/UserController.java
+++ src/main/java/com/avigya/userbase/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.avigya.userbase.controller;
 
+import com.avigya.userbase.model.Users;
 import com.avigya.userbase.records.RegisteredUser;
 import com.avigya.userbase.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @Slf4j
@@ -20,11 +25,28 @@ public class UserController {
     private String createUser(@RequestBody RegisteredUser user)
     {
         log.info("User create request received");
-        return usersService.createUser(user);
+        return usersService.createUser(user).toString();
     }
-
-    private String createUser( )
+    @GetMapping("/getUser")
+    private ResponseEntity<?> getUser(@RequestBody RegisteredUser user)
     {
-        return "";
+        try
+        {
+            Users userFound=usersService.getRegisteredUser(user);
+            return new ResponseEntity<>(userFound,HttpStatus.OK);
+        } catch (RuntimeException e)
+        {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+
+    }
+    @PostMapping("/saveUser")
+    private ResponseEntity<String> saveUserDetails(@RequestBody Users user) {
+        try {
+            return new ResponseEntity<>(usersService.saveUser(user), HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
     }
 }
